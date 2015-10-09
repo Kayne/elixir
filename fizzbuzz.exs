db285bb18c2b1f974f6b9f5f3d63df07cf3dd11b
Marcin Bratek <marcin.bratek@sharewise.com>
2015-10-09 20:21:02 +0200

    fizzbuzz with rem

--- fizzbuzz.exs
+++ fizzbuzz.exs
@@ -10,4 +10,12 @@ IO.puts fizzbuzz_1.(0,213,132)
 IO.puts fizzbuzz_1.("abc", 0, 213)
 IO.puts fizzbuzz_1.("a","b","ostatni")
 
+fizzbuzz_2 = fn (n) -> fizzbuzz_1.( rem(n, 3), rem(n, 5), n) end
 
+IO.puts fizzbuzz_2.(10)
+IO.puts fizzbuzz_2.(11)
+IO.puts fizzbuzz_2.(12)
+IO.puts fizzbuzz_2.(13)
+IO.puts fizzbuzz_2.(14)
+IO.puts fizzbuzz_2.(15)
+IO.puts fizzbuzz_2.(16)
