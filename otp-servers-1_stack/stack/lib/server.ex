b06e987bad8cef50117513d916b0b4f1c277fb2b
Kayne <github@bratekmarcin.eu>
2016-08-26 21:31:29 +0200

    external api for GenServer

--- otp-servers-1_stack/stack/lib/server.ex
+++ otp-servers-1_stack/stack/lib/server.ex
@@ -1,6 +1,26 @@
 defmodule Stack.Server do
   use GenServer
 
+  ###
+  # External API
+  ###
+
+  def start_link( stack ) do
+  	GenServer.start_link(__MODULE__, stack, name: __MODULE__)
+  end
+
+  def pop do
+  	GenServer.call __MODULE__, :pop
+  end
+
+  def push( item ) do
+  	GenServer.cast __MODULE__, { :push, item }
+  end
+
+  ###
+  # GenServer implementation
+  ###
+
   def handle_call( :pop, _from, stack ) do
     [ head | tail ] = stack
     { :reply, head, tail }
