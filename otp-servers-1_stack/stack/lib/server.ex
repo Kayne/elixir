0b8a022a25ca039c964b1bfd0c445281d71664fa
Kayne <github@bratekmarcin.eu>
2016-08-26 23:44:20 +0200

    implemented terminate callback

--- otp-servers-1_stack/stack/lib/server.ex
+++ otp-servers-1_stack/stack/lib/server.ex
@@ -6,15 +6,15 @@ defmodule Stack.Server do
   ###
 
   def start_link( stack ) do
-  	GenServer.start_link(__MODULE__, stack, name: __MODULE__)
+    GenServer.start_link(__MODULE__, stack, name: __MODULE__)
   end
 
   def pop do
-  	GenServer.call __MODULE__, :pop
+    GenServer.call __MODULE__, :pop
   end
 
   def push( item ) do
-  	GenServer.cast __MODULE__, { :push, item }
+    GenServer.cast __MODULE__, { :push, item }
   end
 
   ###
@@ -27,7 +27,12 @@ defmodule Stack.Server do
   end
 
   def handle_cast( { :push, item }, stack ) do
-  	{ :noreply, [ item | stack ] }
+    { :noreply, [ item | stack ] }
+  end
+
+  def terminate( reason, state ) do
+    IO.puts "Asked to stop because #{inspect reason}"
+    :ok
   end
 end
 
